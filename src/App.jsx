16b60ab3b135c8592a83a8a3113ca4e0833b7ec3
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-12 18:29:46 -0300

    add: aleatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 
-export default _ =>
+export default () =>
     <div id="app">
-        <h1>Fundamentos React (Arrow)</h1>
+        <h1>Fundamentos React</h1>
+
+        <Aleatorio min={1} max={60}/>
         <Fragmento />
         <ComParametro
             titulo="Situação do Aluno"
