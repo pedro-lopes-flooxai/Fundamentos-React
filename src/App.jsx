22439d0bd1eae2657ecd9d037afb5eea93a3f0f0
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-12 18:58:35 -0300

    add: css

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,32 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import Card from './components/layout/Card';
 
 export default () =>
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Card titulo=" #4 Desafio Aleatório">
+            <Aleatorio min={1} max={60} />
+        </Card>
 
-        <Aleatorio min={1} max={60}/>
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro"
-            nota={9.3}
-        />
-        <Primeiro></Primeiro>
+        <Card titulo=" #3 Fragmento">
+            <Fragmento/>
+        </Card>
+        <Card titulo="Exemplo de Card"></Card>
+
+        
+        <Card titulo=" #2 Com Parâmetro">
+            <ComParametro
+                titulo="Situação do Aluno"
+                aluno="Pedro"
+                nota={9.3}
+            />
+        </Card>
+
+        <Card titulo=" #1 Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
+        
     </div>
 
