efb1ce6c2edf414927e4152a85f38a8393a882e3
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-12 22:01:04 -0300

    add: list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import './App.css'
 import React from 'react';
 
+import ListaAlunos from './components/repeticao/ListaAlunos';
+import Familia from './components/basicos/Familia';
+import FamiliaMembro from './components/basicos/FamiliaMembro';
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Card from './components/layout/Card';
 
+
 export default () =>
     <div className="App">
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+        <Card titulo=" #6 - Repetição" color="#FF4C65">
+            <ListaAlunos></ListaAlunos>
+        </Card>
+
+        <Card titulo=" #5 Componente com Filhos" color="#00C8F8">
+            <Familia sobrenome ="Silva">
+                <FamiliaMembro nome ="Pedro"/>
+               <FamiliaMembro nome ="Ana"/>
+                <FamiliaMembro nome ="Gustavo" /> 
+            </Familia>
+        </Card>
 
         <Card titulo=" #4 Desafio Aleatório" color="#FA6900">
             <Aleatorio min={1} max={60} />
---
+++ src/data/alunos.jsx
@@ -0,0 +1,12 @@
+export default [
+    { id: 1, nome: 'Ana', nota: 6.2 },
+    { id: 2, nome: 'Bia', nota: 7.6 },
+    { id: 3, nome: 'Carlos', nota: 8.2 },
+    { id: 4, nome: 'Daniel', nota: 5.2 },
+    { id: 5, nome: 'Gui', nota: 9.2 },
+    { id: 6, nome: 'Rebeca', nota: 9.5 },
+    { id: 7, nome: 'Arthur', nota: 6.9 },
+    { id: 8, nome: 'Pedro', nota: 10.0 },
+    { id: 9, nome: 'Gustavo', nota: 6.2 },
+
+]
